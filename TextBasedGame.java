e83cf3ae63049526bba654f3eb91f92c862ce96e
bennie-he <45060944+bennie-he@users.noreply.github.com>
2019-07-19 21:59:57 -0600

    skeleton

---
+++ TextBasedGame.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class TextBasedGame {
+	private int[][] board;
+	private int highScore;
+	private int currentScore;
+	private Scanner scanner;
+	
+	public TextBasedGame() {
+		
+	}
+	
+	public void generateNewTile() {
+		
+	}
+	
+	public void computeMovement() {
+		
+	}
+	
+	public void moveUp() {
+		
+	}
+	
+	public void moveDown() {
+		
+	}
+	
+	public void moveLeft() {
+	
+	}
+	
+	public void moveRight() {
+	
+	}
+	
+	public void checkCollisions() {
+		
+	}
+	
+	public void displayBoard() {
+		
+	}
+	
+	public void victoryScreen() {
+		
+	}
+	
+	public void defeatScreen() {
+		
+	}
+}
