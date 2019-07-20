f57ace8fe4b16c478bfeabcfe931d4269d321f13
bennie-he <45060944+bennie-he@users.noreply.github.com>
2019-07-19 22:42:23 -0600

    updated with getter/setter logic and constructor logic

--- TextBasedGame.java
+++ TextBasedGame.java
@@ -7,18 +7,42 @@ public class TextBasedGame {
 	private Scanner scanner;
 	
 	public TextBasedGame() {
+		int[][] temp=new int[4][4];
+		for(int i=0;i<4;i++) {
+			for(int j=0;j<4;j++) {
+				board[i][j]=0;
+			}
+		}
+		setBoard(temp);
+		setHighScore(0);
+		setCurrentScore(0);
+		scanner=new Scanner(System.in);
 		
+		generateNewTile();
+		while(!victoryScreen()&&!defeatScreen()) {
+			generateNewTile();
+			displayBoard();
+			computeMovement();
+		}
+		if(victoryScreen()) {
+			
+		}
+			
+		if(defeatScreen()) {
+			
+		}
 	}
-	
+	//josh
 	public void generateNewTile() {
 		
 	}
-	
+	//josh
 	public void computeMovement() {
 		
 	}
-	
+	//bennie
 	public void moveUp() {
+		
 		
 	}
 	
@@ -33,20 +57,61 @@ public class TextBasedGame {
 	public void moveRight() {
 	
 	}
-	
-	public void checkCollisions() {
+	//bennie
+	public int[] checkCollisions(int[] line, char dir) {
 		
+		return null;
 	}
-	
+	//jason
 	public void displayBoard() {
 		
 	}
-	
-	public void victoryScreen() {
-		
+	//jason
+	public boolean victoryScreen() {
+		return false;
 	}
-	
-	public void defeatScreen() {
-		
+	//ammar
+	public boolean defeatScreen() {
+		return false;
+	}
+
+	public int[][] getBoard() {
+		int[][] temp=new int[4][4];
+		for(int i=0;i<4;i++) {
+			for(int j=0;j<4;j++) {
+				temp[i][j]=board[i][j];
+			}
+		}
+		return temp;
+	}
+
+	public int getHighScore() {
+		return highScore;
+	}
+
+	public int getCurrentScore() {
+		return currentScore;
+	}
+
+	public Scanner getScanner() {
+		return scanner;
+	}
+
+	public void setBoard(int[][] b) {
+		for(int i=0;i<4;i++) {
+			for(int j=0;j<4;j++) {
+				board[i][j]=b[i][j];
+			}
+		}
+	}
+
+	public void setHighScore(int h) {
+		if(h>=0&&h>=getCurrentScore())
+			this.highScore = h;
+	}
+
+	public void setCurrentScore(int c) {
+		if(c>=0)
+			this.currentScore = c;
 	}
 }
