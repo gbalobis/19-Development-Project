7782f4845931b6638c0fb945e1e2a83fe3ce35b8
bennie-he <45060944+bennie-he@users.noreply.github.com>
2019-07-21 22:12:59 -0600

    update
    added notes for startGame function
    edited logic in the case of a victory

--- TextBasedGame.java
+++ TextBasedGame.java
@@ -22,19 +22,29 @@ public class TextBasedGame {
 	
 	public void startGame() {
 		while(true) {
+			//set up board with 2 new tiles and display it in the console
 			generateNewTile();
+			generateNewTile();
+			displayBoard();
+			//while the board is not in a victory or defeat screen, repeat following functions
 			while(!victoryCheck()&&!defeatCheck()) {
+				//configure board based on movement inputed
+				computeMovement();
+				//create a new tile in an empty space and display board in console
 				generateNewTile();
 				displayBoard();
-				computeMovement();
 			}
 			char cont='z';
+			//if board is in a defeat state, ask player if they would like to restart
 			if(defeatCheck()) {
-				System.out.println("Would you like to restart? (y / n)");
+				System.out.println("DEFEAT!!!!\n Would you like to restart? (y / n)");
+				//wait for a valid response of y or n
 				while(cont!='y'&&cont!='n')
 					cont=getScanner().next().charAt(0);
+				//if they do not restart, end the function
 				if(cont=='n')
 					return;
+				//if they do restart, reset board and current score, but not high score
 				if(cont=='y') {
 					int[][] temp=new int[4][4];
 					for(int i=0;i<4;i++) {
@@ -46,16 +56,26 @@ public class TextBasedGame {
 					setCurrentScore(0);
 				}
 			}
-		
+			//if board is in a victory state, ask player if they would like to continue in endless mode
 			if(victoryCheck()) {
-				System.out.println("Would you like to continue? (y / n)");
+				System.out.println("VICTORY!!!!\n Would you like to continue? (y / n)");
+				//wait for a valid response of y or n
 				while(cont!='y'&&cont!='n')
 					cont=getScanner().next().charAt(0);
+				//if they do not continue, end the function
 				if(cont=='n') {
 					return;
 				}
+				//if they do continue, ignore this check
 				if(cont=='y') {
-					continue;
+					//continue the game, checking only for a defeat
+					while(!defeatCheck()) {
+						//configure board based on movement inputed
+						computeMovement();
+						//create a new tile in an empty space and display board in console
+						generateNewTile();
+						displayBoard();
+					}
 				}
 			}
 		}
@@ -198,7 +218,9 @@ public class TextBasedGame {
 			for (int positionY = 0; positionY < 4; positionY++) {
 				System.out.print(getBoard()[positionX][positionY]+ " ");
 			}
+			System.out.println();
 		}
+		System.out.println();
 	}
 	/*
 	 * This method checks the board if the tile "2048" is present and returns true if it is found and false otherwise.  
